2c9156d41bd69bb710c26176dc5fa1ffad8d8291
luis guevara <luis_23g@hotmail.com>
2021-08-02 16:24:19 -0600

    CSV testing

---
+++ CovidUnitTest/Utilities/CsvHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CovidApp.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidApp.Utilities.Tests
+{
+    [TestClass()]
+    public class CsvHelperTests
+    {
+        [TestMethod()]
+        public void ConvertToCsvConvertsDataWell()
+        {
+            var obj = new List<Object>() { { new { Name = "Luis", LastName = "Guevara" } }, { new { Name = "Raquel", LastName = "Guevara" } } };            
+            string csvExpectedResult = "Name,LastName\nLuis,Guevara\nRaquel,Guevara";
+            //
+            string csvResult = CsvHelper.ConvertToCsv(obj);
+            bool valid = csvExpectedResult == csvResult;
+            //
+             Assert.IsTrue(valid);
+        }
+    }
+}
